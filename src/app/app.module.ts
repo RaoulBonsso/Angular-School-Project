ed522c25d7813e0293b46b43087d84505d81b2b9
goldspide <raoulbonsso@gmail.com>
2025-01-15 16:55:32 +0100

    exercise 6 et 7 terminé

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from'@angular/forms';
 import { CoursesComponent } from './courses/courses.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
